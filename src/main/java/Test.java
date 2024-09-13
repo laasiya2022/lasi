9d06153a43bde5bcf7666fb43bbd4cdcf831f8d8
day <laasiya.sivanandam@gmail.com>
2024-09-13 12:36:10 +0530

    code added

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -9,6 +9,17 @@ private void method2() {
 }
 private void method3() {
 	System.out.println("C lang");
+	}
+private void sql() {
+	System.out.println("sql is east");
+
+}
+private void python() {
+	System.out.println("python is tough");
+
+}
+private void cLang() {
+	System.out.println("C language");
 
 }
 }
