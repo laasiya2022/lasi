4d3419f97d9e2c2a78b94da41fb188654a3d9edc
day <laasiya.sivanandam@gmail.com>
2024-09-13 12:10:30 +0530

    message

---
+++ src/main/java/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+	private void method1() {
+		System.out.println("java");
+			}
+private void method2() {
+	System.out.println("sql");
+
+}
+private void method3() {
+	System.out.println("C lang");
+
+}
+}
